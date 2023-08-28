10cc4bfc0722e70c3c19f44ad646ed7d76537804
davys <davys@DESKTOP-IMRLCQV>
2023-08-28 01:28:46 -0300

    feat: add basic commands

---
+++ src/CQRS.Core/Messages/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Core.Messages
+{
+    public abstract class Message
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Post.Command.Api/Commands/EditPost.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Commands;
+
+namespace Post.Command.Api.Commands
+{
+    public class EditPost : BaseCommand
+    {
+        public string Message { get; set; }
+    }
+}
