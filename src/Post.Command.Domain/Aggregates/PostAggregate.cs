8eacc5201cb83185e4380792ea8d4d70091064b4
davys <davys@DESKTOP-IMRLCQV>
2023-08-28 23:51:11 -0300

    refac: change EditMessage to UpdateMessage on PostAggregate

--- src/Post.Command.Domain/Aggregates/PostAggregate.cs
+++ src/Post.Command.Domain/Aggregates/PostAggregate.cs
@@ -44,7 +44,7 @@ namespace Post.Command.Domain.Aggregates
             _author = @event.Author;
         }
 
-        public void EditMessage(string message)
+        public void UpdateMessage(string message)
         {
             if(!_active)
             {
