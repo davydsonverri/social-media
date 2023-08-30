5c519541d387a416b56d192d3aec83c366076671
Davydson Santana <1733168+davydsonsantana@users.noreply.github.com>
2023-08-30 00:28:59 -0300

    feat: add kafka integration on EventStore (#9)
    Co-authored-by: davys <davys@DESKTOP-IMRLCQV>

---
+++ src/CQRS.Core/Producers/IEventProducer.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.Core.Messages;
+
+namespace CQRS.Core.Producers
+{
+    public interface IEventProducer
+    {
+        Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent;
+    }
+}
---
+++ src/Post.Command.Infra/Producers/EventProducer.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+using CQRS.Core.Exceptions;
+using CQRS.Core.Messages;
+using CQRS.Core.Producers;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Post.Command.Infra.Producers
+{
+    public class EventProducer : IEventProducer
+    {
+        private readonly ProducerConfig _config;
+
+        public EventProducer(IOptions<ProducerConfig> config)
+        {
+            _config = config.Value;
+        }
+        public async Task ProduceAsync<T>(string topic, T @event) where T : BaseEvent
+        {
+            using var producer = new ProducerBuilder<string, string>(_config)
+                .SetKeySerializer(Serializers.Utf8)
+                .SetValueSerializer(Serializers.Utf8)
+                .Build();
+
+            var eventMessage = new Message<string, string>
+            {
+                Key = Guid.NewGuid().ToString(),
+                Value = JsonSerializer.Serialize(@event, @event.GetType())
+            };
+
+            var deliveryResult = await producer.ProduceAsync(topic, eventMessage);
+
+            if(deliveryResult.Status == PersistenceStatus.NotPersisted) 
+            {
+                throw new BrokerException($"Unable to produce {@event.GetType().Name} message on topic {topic} due to: {deliveryResult.Message}");
+            }
+        }
+    }
+}
