2935b275e5ff002a339048ac1c36f57e95df5912
davys <davys@DESKTOP-IMRLCQV>
2023-08-29 21:21:43 -0300

    feat: add event sourcing handler

---
+++ src/CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -0,0 +1,10 @@
+﻿using CQRS.Core.Domain;
+
+namespace CQRS.Core.Handlers
+{
+    public interface IEventSourcingHandler<T>
+    {
+        Task SaveAsync(AggregateRoot aggregate);
+        Task<T> GetByIdAsync(Guid aggregateId);
+    }
+}
--- src/Post.Command.Api/Program.cs
+++ src/Post.Command.Api/Program.cs
@@ -1,12 +1,15 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Infra;
 using Post.Command.Infra.Config;
 using Post.Command.Infra.Repositories;
+using Post.Command.Infra.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddScoped<IEventStore, EventStore>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
