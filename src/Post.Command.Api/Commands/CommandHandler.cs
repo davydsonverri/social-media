b0c37df6d25dbc4a330018518aec203c639fc029
davys <davys@DESKTOP-IMRLCQV>
2023-08-29 22:55:26 -0300

    fix: add command handler

---
+++ src/Post.Command.Api/Commands/CommandHandler.cs
@@ -0,0 +1,63 @@
+﻿using CQRS.Core.Handlers;
+using Post.Command.Domain.Aggregates;
+
+namespace Post.Command.Api.Commands
+{
+    public class CommandHandler : ICommandHandler
+    {
+        private readonly IEventSourcingHandler<PostAggregate> _eventSourcingHandler;
+
+        public CommandHandler(IEventSourcingHandler<PostAggregate> eventSourcingHandler)
+        {
+            _eventSourcingHandler = eventSourcingHandler;
+        }
+
+        public async Task HandleAsync(CreatePost command)
+        {
+            var aggregate = new PostAggregate(command.Id, command.Author, command.Message);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(UpdatePost command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.UpdateMessage(command.Message);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(DeletePost command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.DeletePost(command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(LikePost command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.LikePost();
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(CommentPost command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.AddComment(command.Comment, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(UpdateComment command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.UpdateComment(command.CommentId, command.Comment, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+
+        public async Task HandleAsync(DeleteComment command)
+        {
+            var aggregate = await _eventSourcingHandler.GetByIdAsync(command.Id);
+            aggregate.DeleteComment(command.CommentId, command.Username);
+            await _eventSourcingHandler.SaveAsync(aggregate);
+        }
+    }
+}
--- src/Post.Command.Api/Program.cs
+++ src/Post.Command.Api/Program.cs
@@ -1,6 +1,11 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infra;
+using Post.Command.Api.Commands;
+using Post.Command.Domain.Aggregates;
 using Post.Command.Infra.Config;
+using Post.Command.Infra.Dispatchers;
+using Post.Command.Infra.Handlers;
 using Post.Command.Infra.Repositories;
 using Post.Command.Infra.Stores;
 
@@ -10,6 +15,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
+
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+var dispatcher = new CommandDispatcher();
+dispatcher.RegisterHandler<CreatePost>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<UpdatePost>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeletePost>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<LikePost>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<CommentPost>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<UpdateComment>(commandHandler.HandleAsync);
+dispatcher.RegisterHandler<DeleteComment>(commandHandler.HandleAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
