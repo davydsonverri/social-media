90d7371fc4cfa4c60cd0de048d98e97f6495d4f8
davys <davys@DESKTOP-IMRLCQV>
2023-09-03 17:01:00 -0300

    feat: add query handlers

---
+++ src/Post.Query.Api/Queries/QueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Post.Query.Domain.Entities;
+using Post.Query.Domain.Repositories;
+
+namespace Post.Query.Api.Queries
+{
+    public class QueryHandler : IQueryHandler
+    {
+        private readonly IPostRepository _postRepository;
+        public QueryHandler(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public async Task<List<PostEntity>> HandleAsync(ListAllPostsQuery query)
+        {
+            return await _postRepository.ListAllAsync();
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostByAuthorQuery query)
+        {
+            return await _postRepository.ListByAuthor(query.Author);
+        }
+
+        public async Task<List<PostEntity>> HandleAsync(FindPostByIdQuery query)
+        {
+            var post = await _postRepository.GetByIdAsync(query.Id);
+            if (post != null)
+                return new List<PostEntity>() { post };
+            else
+                return new List<PostEntity>();
+        }
+    }
+}
